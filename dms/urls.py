4a7e9f348d829e6550801677a6f137bf8c8f366b
nueldeli <nueljoel34@gmail.com>
2021-05-14 16:53:26 +0800

    updated data

--- dms/urls.py
+++ dms/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path 
-from .views import seedling_overview, SeedlingIndexView, SeedlingDetailView, SeedlingAddView, SeedlingUpdateView, SeedlingDeleteView
+from .views import seedling_overview, sabal_index, niah_index, semengoh_index, SeedlingIndexView, SeedlingDetailView, SeedlingAddView, SeedlingUpdateView, SeedlingDeleteView
 
 urlpatterns = [
 	path('', seedling_overview, name='seedling_overview'),
+	path('sabal_index/', sabal_index, name='sabal_index'),
+	path('niah_index/', niah_index, name='niah_index'),
+	path('IFRC_index/', semengoh_index, name='semengoh_index'),
 	path('seedling_index/', SeedlingIndexView.as_view(), name='seedling_index'),
 	path('seedling_detail/<int:pk>/', SeedlingDetailView.as_view(), name='seedling_detail'),
 	path('seedling_add/', SeedlingAddView.as_view(), name='seedling_add'),
--- dms/views.py
+++ dms/views.py
@@ -5,9 +5,10 @@ from .forms import AddSeedlingForm, UpdateSeedlingForm
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.db.models import Sum
 
+data_object = Seedling.objects.all()
+
 # Create your views here.
 def seedling_overview(request):
-	data_object = Seedling.objects.all()
 	sabal_total_species = data_object.filter(nursery__icontains='Sabal').count()
 	niah_total_species = data_object.filter(nursery__icontains='Niah').count()
 	semengoh_total_species = data_object.filter(nursery__icontains='IFRC').count()
@@ -24,6 +25,18 @@ def seedling_overview(request):
 		'label':label,
 		'data':data
 		})
+
+def sabal_index(request):
+	sabal_nursery_index = data_object.filter(nursery__icontains='Sabal')
+	return render(request, 'dms/sabal_index.html', {'sabal_nursery_index':sabal_nursery_index})
+
+def niah_index(request):
+	niah_nursery_index = data_object.filter(nursery__icontains='Niah')
+	return render(request, 'dms/niah_index.html', {'niah_nursery_index':niah_nursery_index})
+
+def semengoh_index(request):
+	semengoh_nursery_index = data_object.filter(nursery__icontains='IFRC')
+	return render(request, 'dms/semengoh_index.html', {'semengoh_nursery_index':semengoh_nursery_index})
 
 class SeedlingIndexView(ListView):
 	model = Seedling
